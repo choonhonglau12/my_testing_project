2c79e35600594e12ead8970c3a7a0031be56d234
choonhonglau12 <choonhonglau12@gmail.com>
2025-06-05 22:50:25 +0800

    make changes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__ , template_folder='docs')
 
 @app.route("/")
 def home():
     return render_template("index.html")
+    # return "<h1>Hello, Flask is working!</h1>"
 
 if __name__ == "__main__":
+    print("Starting Flask app...")
     app.run(debug=True)
